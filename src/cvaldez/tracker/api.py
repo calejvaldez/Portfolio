528a3bdc45356d9c17659e81571b2a17b1d0461a
Carlos Valdez <cvaldezh@uci.edu>
2023-05-03 14:56:27 -0700

    feat: added URL prefixes

--- src/cvaldez/tracker/api.py
+++ src/cvaldez/tracker/api.py
@@ -2,7 +2,8 @@ from flask import Blueprint
 
 bp = Blueprint('tracker api', __name__,
                template_folder='templates',
-               static_folder='static')
+               static_folder='static',
+               url_prefix='/tracker/api/')
 
 
 @bp.route('/')
--- src/cvaldez/tracker/views.py
+++ src/cvaldez/tracker/views.py
@@ -2,7 +2,8 @@ from flask import Blueprint
 
 bp = Blueprint('tracker', __name__,
                template_folder='templates',
-               static_folder='static')
+               static_folder='static',
+               url_prefix='/tracker/')
 
 
 @bp.route('/')
