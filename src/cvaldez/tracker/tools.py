b51f0386f7cc1873845036885ba604d2353efa87
Carlos Valdez <cvaldezh@uci.edu>
2023-05-03 19:20:46 -0700

    feat: added more minor details, still working on things

--- src/cvaldez/tracker/tools.py
+++ src/cvaldez/tracker/tools.py
@@ -2,18 +2,52 @@
 # Carlos Valdez
 #
 # These are tools for the Project Status Tracker.
+import sqlite3
+import psycopg2
+import os
+from collections import namedtuple
+
+Student = namedtuple('Student', ['name', 'age', 'DOB'])
+ProjectTuple = namedtuple('Project', ['name', 'description', 'id'])
+
+
+class ProjectNotFoundError(Exception):
+    ...
+
+
+def get_project(uuid=None, name=None) -> ProjectTuple:
+    """
+    Find a project inside the database.
+
+    :param uuid: The UUID of the project
+    :param name: The name of the project, case-sensitive
+    :return: A tuple representing a row in the database
+    """
+    assert uuid or name, "get_project: UUID or name is required to find a project."
+
+    with psycopg2.connect(os.getenv("DB_LINK")) as con:
+        cur = con.cursor()
+        if uuid:
+            cur.execute('SELECT * FROM projects WHERE uuid=%s;', (uuid,))
+        else:
+            cur.execute('SELECT * FROM projects WHERE name=%s;', (name,))
+
+        rows = cur.fetchall()
+        if len(rows) == 1:
+            return ProjectTuple(*rows[0])
+        else:
+            raise ProjectNotFoundError()
+
 
 class Project:
     def __init__(self, uuid=None, name=None):
         assert uuid or name, "Project.__init__: UUID or name is required to find a project."
-        if uuid:
-            ... # find from database
-        elif name:
-            ... # find from database
 
-        self.name: str
-        self.description: str
-        self.id: int
+        p = get_project(uuid=uuid, name=name)
+
+        self.name = p.name
+        self.description = p.description
+        self.id = p.id
         self.version: str
         self.last_updated: float
         self.link: str
@@ -26,5 +60,3 @@ class Project:
     def create(name: str, desc: str) -> 'Project':
         ... # add to database
         return Project(...) # uuid
-
-
