25dd2bc3415f8346c3ed921832f07229e8a215a8
Carlos Valdez <cvaldezh@uci.edu>
2023-05-09 22:35:57 -0700

    tests: started with tests, but not working
    I thought I committed the project table to the database, but I guess not. I'll figure this out soon.

---
+++ src/cvaldez/tracker/test_tools.py
@@ -0,0 +1,27 @@
+import unittest
+from tools import create_project
+import psycopg2
+import os
+
+
+class MyTestCase(unittest.TestCase):
+    def test_project_creation(self):
+        p = create_project('Test', 'This is a test project')
+
+        try:
+            self.assertEqual((p.name, p.description, p.version, p.last_updated, p.link, p.icon, p.status),
+                             ('Test', 'This is a test project', None, None, None, 'building.svg', '1'))
+        finally:
+            with psycopg2.connect(os.getenv("DB_LINK")) as con:
+                cur = con.cursor()
+                cur.execute("DELETE FROM pst_projects WHERE id=%s", (p.id,))
+
+                cur.execute("SELECT * FROM pst_projects WHERE id=%s", (p.id,))
+
+                self.assertEqual(len(cur.fetchall()), 0)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
