e56eb6d8d1dd0879c7467304de454630b80f45ac
Carlos Valdez <cvaldezh@uci.edu>
2023-05-13 15:33:49 -0700

    feat: created a buildProject function
    This function will be used to easily create projects instantly. It does need an IF STATEMENT inside to tell apart the statuses.

--- src/cvaldez/tracker/static/scripts/index.ts
+++ src/cvaldez/tracker/static/scripts/index.ts
@@ -2,6 +2,7 @@
 // Carlos Valdez
 // 
 // Some tools that will be needed for the Tracker.
+let div_project = document.getElementById('projects');
 
 
 // Interfaces for Update, Process, and Project that will be returned from the server.
@@ -30,3 +31,78 @@ interface Project {
     release_notes: Array<Update>
     process: Process
 }
+
+function buildProject(data: Project): HTMLAnchorElement {
+    // Builds all the HTML elements to create a project container.
+
+    // Creating the clickable
+    let a = document.createElement('a');
+    a.href = data.link;
+    a.className = 'project-clickable';
+
+    // creating the container
+    let container = document.createElement('div');
+    container.className = 'project-container';
+
+    // project status
+    let status = document.createElement('div')
+    let status_p = document.createElement('p');
+
+    // INSERT IF STATEMENT HERE
+    // INSERT IF STATEMENT HERE
+    // INSERT IF STATEMENT HERE
+
+    status.className = 'project-status-design';
+    status_p.textContent = 'Developing...';
+    status.appendChild(status_p);
+
+    // creating image
+    let img = document.createElement('img');
+    img.className = "img";
+    img.src = `static/images/${data.icon}`;
+    img.height = 150;
+    img.width = 150;
+    img.alt = `${data.name} logo`;
+
+    // creating text/version
+    let text = document.createElement('div');
+    text.className = 'desc-text';
+    let title = document.createElement('h3');
+    title.textContent = data.name;
+    let v = document.createElement('p');
+    v.textContent = `(${data.version})`;
+    v.className = 'v-desc-text';
+    text.appendChild(title);
+    text.appendChild(v);
+
+    // description
+    let desc = document.createElement('p');
+    desc.textContent = data.description;
+
+    // finalizing
+    container.appendChild(status);
+    container.appendChild(img);
+    container.appendChild(text);
+    container.appendChild(desc);
+    a.appendChild(container);
+
+    return a
+    
+};
+
+// This is a sample of what the API would return
+const sample_data: Project = {
+    name: "Carlos' Portfolio",
+    description: "View the status of all of Carlos' projects.",
+    id: "2",
+    version: "Preview",
+    last_updated: "",
+    link: "https://cvaldez.dev/tracker/",
+    icon: "tracker.svg",
+    status: "Developing",
+    release_notes: [],
+    process: {requirements: null, design: null, code: null, updates: null}
+}
+
+// Ideally, this would be a .forEach
+div_project.appendChild(buildProject(sample_data));
